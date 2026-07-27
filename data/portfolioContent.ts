e8cb6f810d223c6d5c8d5d1359a8998c41cbddde
ionut-grigore99 <ionut.orlando@yahoo.com>
2026-07-27 13:14:14 +0300

    awb research added

--- data/portfolioContent.ts
+++ data/portfolioContent.ts
@@ -32,7 +32,7 @@ export const experiences: ExperienceItem[] = [
     ],
     documents: [
       { title: "3D-2D Alignment Report", href: "https://ionut-grigore99.github.io/ionut-grigore/htmls/alignment_report_dino_final.html" },
-      // { title: "Automatic White-Balance Correction Report", href: "https://ionut-grigore99.github.io/ionut-grigore/htmls/papers_comparison_white_balance.html" },
+      { title: "Automatic White-Balance Correction Report", href: "https://ionut-grigore99.github.io/ionut-grigore/htmls/papers_comparison_white_balance.html" },
     ],
   },
   {
